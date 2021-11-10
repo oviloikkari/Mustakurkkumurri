d712931926d2f3a54aebc798c7b6b0d4d1f60f45
Aleksi Annola <dev0n_p@hotmail.com>
2021-11-10 10:56:38 +0200

    eka

---
+++ Planeetta_ground_scene/Assets/loadLevel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadLevel : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
